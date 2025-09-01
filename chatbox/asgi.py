2c967498535cb6be00939a1710ece23ae593750d
Sudharsan Reddy <sudharsanreddy.saragada@gmail.com>
2025-09-01 10:05:43 +0530

    Fix WebSocket for HTTPS + add HTTP in ASGI

--- chatbox/asgi.py
+++ chatbox/asgi.py
@@ -8,19 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+from channels.routing import get_default_application
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import cb.routing  # ✅ import your app’s routing.py
+from channels.routing import ProtocolTypeRouter, URLRouter
+import cb.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbox.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbox.settings")
 
-# Main ASGI application
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),   # Django views
-    "websocket": AuthMiddlewareStack(  # WebSocket handler
-        URLRouter(
-            cb.routing.websocket_urlpatterns   # from cb/routing.py
-        )
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(cb.routing.websocket_urlpatterns)
     ),
 })
