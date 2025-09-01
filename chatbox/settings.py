02d0b57350e7e39a745b7408c11a80efd725132c
Sudharsan Reddy <sudharsanreddy.saragada@gmail.com>
2025-09-01 23:37:03 +0530

    settings.py update

--- chatbox/settings.py
+++ chatbox/settings.py
@@ -1,12 +1,15 @@
 from pathlib import Path
-from decouple import config  # already installed
+from decouple import config  # make sure python-decouple is installed
 
+# --- Base Directory ---
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# --- Security ---
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="").split(",")
 
+# --- Installed Apps ---
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -14,18 +17,46 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "cb",
-    "channels",
+    "cb",          # your chat app
+    "channels",    # Django Channels
 ]
 
+# --- Middleware ---
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+# --- URL Configuration ---
+ROOT_URLCONF = "chatbox.urls"  # <- THIS IS IMPORTANT
+
+# --- Templates ---
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+# --- WSGI / ASGI ---
+WSGI_APPLICATION = "chatbox.wsgi.application"
 ASGI_APPLICATION = "chatbox.asgi.application"
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
-}
-
+# --- Database ---
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -33,5 +64,38 @@ DATABASES = {
     }
 }
 
-STATIC_URL = "static/"
+# --- Channels ---
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    }
+}
+
+# --- Password validation ---
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+# --- Internationalization ---
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+# --- Static files ---
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# --- Default auto field ---
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
