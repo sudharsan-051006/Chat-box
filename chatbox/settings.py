bfbfdbbf074d8909436d058ae6fe649db9c79fa2
Sudharsan Reddy <sudharsanreddy.saragada@gmail.com>
2025-09-02 17:58:49 +0530

    Update to redis

--- chatbox/settings.py
+++ chatbox/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config  # make sure python-decouple is installed
+import os
 
 # --- Base Directory ---
 BASE_DIR = Path(__file__).resolve().parent.parent
