d3b4c079993dc445144884b7ba1cdbb9c20eae7c
sudharsan-051006 <sudharsanreddy.saragada@gmail.com>
2025-11-05 22:19:10 +0530

    Refactor URL patterns for chat system

--- cb/urls.py
+++ cb/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", auth_views.LoginView.as_view(template_name="cb/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path('', lambda request: redirect('chat/room1/')),
-    path("chat/<str:room_name>/", views.room, name="room"),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='cb/password_change.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='cb/password_change_done.html'), name='password_change_done'),
-    path('create/', views.create_room, name='create_room'),
-    path('chat/<str:room_name>/', views.room, name='room'),
+
+    # Chat system
+    path('', views.index, name='index'),                         # 👈 Room list (create_room.html)
+    path('create/', views.create_room, name='create_room'),       # 👈 Create new room
+    path('chat/<str:room_name>/', views.room, name='room'),    
 ]
