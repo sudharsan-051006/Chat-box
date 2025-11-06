c98b5c19a802147cf8a88fadac6913af37b094b3
sudharsan-051006 <sudharsanreddy.saragada@gmail.com>
2025-11-06 20:53:43 +0530

    Update models.py

--- cb/models.py
+++ cb/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Room(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_rooms")
+    is_locked = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
