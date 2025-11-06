48bbabfdbc78760945568bbf9344a6a9d58b1e19
sudharsan-051006 <sudharsanreddy.saragada@gmail.com>
2025-11-06 21:47:26 +0530

    Update models.py

--- cb/models.py
+++ cb/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
 class Room(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=255, unique=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_rooms")
     is_locked = models.BooleanField(default=False)
 
