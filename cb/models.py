409511ee6970f021fd7a6ce9c36f8e5ae1a2e5e0
sudharsan-051006 <sudharsanreddy.saragada@gmail.com>
2025-11-07 09:37:48 +0530

    Update models.py

--- cb/models.py
+++ cb/models.py
@@ -7,6 +7,7 @@ class Room(models.Model):
     name = models.CharField(max_length=255, unique=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_rooms")
     is_locked = models.BooleanField(default=False)
-
+    allowed_users = models.ManyToManyField(User, related_name="joined_rooms", blank=True)
+    
     def __str__(self):
         return self.name
