604453ee355cfeb1c3c414bd8113db33b7310355
sudharsan-051006 <sudharsanreddy.saragada@gmail.com>
2025-11-07 10:05:53 +0530

    Update views.py

--- cb/views.py
+++ cb/views.py
@@ -11,6 +11,7 @@ from django.contrib import messages
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.db import connection
+from django.http import JsonResponse
 
 
 
