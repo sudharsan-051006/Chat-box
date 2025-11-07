7f9a8d0df31b39fd1f14984b3a046d7c2b837f87
sudharsan-051006 <sudharsanreddy.saragada@gmail.com>
2025-11-07 10:45:53 +0530

    Update views.py

--- cb/views.py
+++ cb/views.py
@@ -96,10 +96,10 @@ def toggle_lock(request, room_name):
 #         return HttpResponse("✅ cb_room_allowed_users table created successfully!")
 #     except Exception as e:
 #         return HttpResponse(f"❌ Error while creating table: {e}")
-# # def run_migrations(request):
-#     try:
-#         call_command('makemigrations', 'cb')
-#         call_command('migrate')
-#         return HttpResponse("✅ Migrations applied successfully.")
-#     except Exception as e:
-#         return HttpResponse(f"❌ Migration error: {str(e)}")
+def run_migrations(request):
+    try:
+        call_command('makemigrations', 'cb')
+        call_command('migrate')
+        return HttpResponse("✅ Migrations applied successfully.")
+    except Exception as e:
+        return HttpResponse(f"❌ Migration error: {str(e)}")
