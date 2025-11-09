f4a05c61ca26720fb7b0114c1ede62dc6ae07095
sudharsan-051006 <sudharsanreddy.saragada@gmail.com>
2025-11-09 17:18:01 +0530

    Update models.py

--- cb/models.py
+++ cb/models.py
@@ -5,9 +5,13 @@ User = get_user_model()
 
 class Room(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_rooms")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     is_locked = models.BooleanField(default=False)
-    allowed_users = models.ManyToManyField(User, related_name="joined_rooms", blank=True)
-    
+
+    # ⚡️ Instead of relying only on allowed_users M2M, also keep usernames
+    allowed_usernames = models.JSONField(default=list, blank=True)
+
+    allowed_users = models.ManyToManyField(User, blank=True, related_name="rooms_allowed")
+
     def __str__(self):
         return self.name
