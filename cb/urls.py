45a95886f7b7105464ef8eab7c5d0f077b63b386
sudharsan-051006 <sudharsanreddy.saragada@gmail.com>
2025-12-31 20:56:26 +0530

    Replace default login view with CustomLoginView

--- cb/urls.py
+++ cb/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import CustomLoginView
 from django.shortcuts import redirect
 
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
-    path("login/", auth_views.LoginView.as_view(template_name="cb/login.html"), name="login"),
+    path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='cb/password_change.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='cb/password_change_done.html'), name='password_change_done'),
